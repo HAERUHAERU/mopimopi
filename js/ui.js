2538c4cb5c5032fcbe5051f0bc1c5ed4c2eb6304
HAERUHAERU <ffxivhaeru@gmail.com>
2018-07-21 10:45:49 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -298,8 +298,10 @@ function duCheckMsg(flag, tab1, tab2) {
         tmp = tmp.slice(0, -2)
 
     $('li#' + flag + 'filter .gVal').text(tmp)
-    update(previewDPS, previewHPS)
-    resizeWindow(view)
+    if(init.q.preview){
+        update(previewDPS, previewHPS)
+        resizeWindow(view)
+    }
 }
 //---------------------------------------------------------------------------------------------------------------- 데이터 출력 순서 얻기
 function getOrder(selector, container) {
