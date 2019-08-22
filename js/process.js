001c22b2153961d1492ab08ff0cf747fff0dab8e
HAERUHAERU <ffxivhaeru@gmail.com>
2019-08-22 19:34:03 +0900

    Update process.js

--- js/process.js
+++ js/process.js
@@ -9,6 +9,7 @@ var barSize = new Array(),
 function onOverlayDataUpdate(e) {
     lastDPS = lastCombat
     lastHPS = new Combatant(e, 'enchps');
+    console.log(lastDPS)
     if (view != 'settings') {
         if (!firstCombat) {
             $('[name=notice], [name=history]').fadeOut(0)
