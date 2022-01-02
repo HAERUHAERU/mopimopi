f5a3570ab8c4aea24022ef1b89e43eefbc210568
HAERUHAERU <ffxivhaeru@gmail.com>
2022-01-02 20:39:40 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -33,12 +33,7 @@ function addOption() {
     if (!init.ColData.Class.hasOwnProperty("width")) {
         init.ColData = Mopi2.ColData;
         init.Order = Mopi2.Order;
-    }
-    
-     //임시기간에만 실행
-    if (init['Color']['RPR'] == "3A3A35")
-        init['Color']['RPR'] = "EADD6F"
-    
+    }    
     SubOption()
 }
 function putValue(arr, c) {
@@ -1132,7 +1127,7 @@ function createElement(type, obj, id, flag) {
         case 'li_text_inbtn': 
             return '<li class="li_text" style="border:0"><table><tr><td rowspan="2" class="gIcon"><i class="material-icons">' + obj.i + '</i></td><td style="width:100%;"><div class="inputBox"><input class="inputEff" type="text" placeholder="' + obj.m[lang] + '" id="' + id + '"><span class="focus-border"></span></div></td><td rowspan="2" class="gIcon ft sendBtn"><i class="material-icons">send</i></td></tr></table></li>'
         case 'li_color': 
-            var input = $('<input id = "' + id + '" value="' + init.Color[id] + '" style="text-align:center; ime-mode:disabled" maxlength="6" onKeyUp="this.value=this.value.toUpperCase();">')
+            var input = $('<input id = "' + id + '" value="' + init.Color[id] + '" style="text-align:center; ime-mode:disabled" maxlength="6">')
             input.addClass("shadow inputEff jscolor {onFineChange:'jsColorUpdate(this)', width:240, height:160, position:'bottom', borderColor:'#212121', insetColor:'#161616', backgroundColor:'#212121'}")
             var li = '<li id="' + id + '" class="li_box"><table><tr><td class="gIcon"><i class="material-icons">' + obj.i + '</i></td><td class="gTitle">' + obj.tt[lang] + '</td><td style="padding:0 1.4rem">' + input.clone().wrapAll("<div/>").parent().html() + '</td></tr></table></li>'
             return li
