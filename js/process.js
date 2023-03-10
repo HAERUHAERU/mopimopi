08a518bf7278e8519b3d25e9a71598c5b9057bff
FlazH2670 <sazvan.kasim.ali@gmail.com>
2023-03-11 00:05:22 +0100

    yes

--- js/process.js
+++ js/process.js
@@ -197,6 +197,7 @@ function onCombatDataUpdate(flag, last) {
                 if (flag == "HPS") {
                     if (init.q.HPS_T == 1 && a.role == 'Tanker' || init.q.HPS_H == 1 && a.role == 'Healer' || init.q.HPS_D == 1 && a.role == 'DPS' || init.q.HPS_C == 1 && a.Job == 'CBO' || init.q.HPS_M == 1 && a.role == 'Crafter' || init.q.HPS_M == 1 && a.role == 'Gathering') {
                         a.healed-=a.overHeal
+                        a.Healed-=a.overHeal
                         a["healed%"]-=a.overHealPct
                         a.healedPct-=a.OverHealPct
                         a.overHeal = 0
