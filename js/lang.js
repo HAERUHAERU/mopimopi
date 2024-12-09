1fd30fede33e3600ca7e19ed8b3b8bcf57e92cd4
HAERUHAERU <ffxivhaeru@gmail.com>
2024-12-09 09:20:12 +0900

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -5,8 +5,8 @@ l = {
             EN: '<b style="color:#ffea00">MopiMopi<br>Experience the Incredible Customizing!</b><br><br><b>Chrome or Safari</b> apps are recommended for mobile environment.<br>PC Users : <b>OverlayProc ❯ MUST Check "AppRegion"</b> to limit the draggable area.<br><br><font style="color:cyan">If you are using FFXIV_ACT_Plugin version 2.0.2.3 or later,<br>ACT → Plugins → FFXIV Settings Tab → <b>Check Disable Combine Pets with Owner</b>.</font><br><br>Please select <b>your language</b> : <a href="#" id="KR" onclick="initOverlay(this.id)">한국어</a> ❘ <a href="#" id="EN" onclick="initOverlay(this.id)">English</a> ❘ <a href="#" id="JP" onclick="initOverlay(this.id)">日本語</a> ❘ <a href="#" id="CN" onclick="initOverlay(this.id)">中國語</a> ❘ <a href="#" id="DE" onclick="initOverlay(this.id)">Deutsch</a> ❘ <a href="#" id="FR" onclick="initOverlay(this.id)">French</a><br>'
         },
         tip: {
-            KR: '[OFFICIAL] MopiMopi ver.20240609 / 해루@모그리',
-            EN: '[OFFICIAL] MopiMopi ver.20240609 / HAERU@Moogle (KR)'
+            KR: '[OFFICIAL] MopiMopi ver.20241209 / 해루@모그리',
+            EN: '[OFFICIAL] MopiMopi ver.20241209 / HAERU@Moogle (KR)'
         },
         update: {
             KR: '<div name="row"><div class="c">05/22</div><div class="m">ACTWebSocket으로 더이상 모피모피를 이용하지 마십시오. OverlayPlugin으로 연결하시기 바랍니다.</div></div><div name="row"><div class="c">05/14</div><div class="m">화면이 변경되지 않는 유저들은 캐시 폴더를 삭제하고 다시 실행해 봅니다.</div></div><div name="row"><div class="c">05/14</div><div class="m">OverlayPlugin : ACT 종료 → 시작 → 실행 → %appdata% → Advanced Combat Tracker → OverlayPluginCache → Cache 폴더 삭제 후 ACT 재실행</div></div>',
@@ -3348,12 +3348,12 @@ l = {
                             CN: "6",
                         },
                         10: {
-                            KR: "10",
-                            JP: "10",
-                            EN: "10",
-                            FR: "10",
-                            DE: "10",
-                            CN: "10",
+                            KR: "12",
+                            JP: "12",
+                            EN: "12",
+                            FR: "12",
+                            DE: "12",
+                            CN: "12",
                         }
                     },
                     e: "li_radio_change",
